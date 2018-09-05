c60a5324460de16df77fa3e7097bc4a8abdba167
chdre <christerdr@outlook.com>
2018-09-05 15:16:54 +0200

    Corrected equations to minimize FLOPS

--- Project1/p1c.cpp
+++ Project1/p1c.cpp
@@ -29,28 +29,13 @@ int main(int argc, char *argv[]){
         }
         double h = 1.0/(n+1); // Step size
 
-        double*x = new double [n+2]; // x_i's
-
-        // Vectors forming the tridiagonal matrix. The upper and lower diagonal are n-1 long
-        double*a = new double [n+1];
-        double*b = new double [n+2];
-        double*c = new double [n+1];
-
-        for(int i = 0; i < n+2; i++) {
-                b[i] = 2.0;
-        }
-        for(int i = 0; i < n+1; i++) {
-                a[i] = -1.0;
-                c[i] = -1.0;
-        }
-
-
         // Vectors for calculations
+        double*x = new double [n+2]; // x_i's
+        double*k = new double [n+2];  // (f tilde)/(b tilde)
         double*u = new double [n+2];
         double*bt = new double [n+2]; // b tilde, diagonal elements (NOT RHS of equation)
         double*ft = new double [n+2]; // f tilde (RHS of equation)
-        double*fprime = new double [n+2];
-
+        double*fprime = new double [n+2]; // our function f multiplied by h²
 
         //loop to update
         for(int i = 0; i < n+2; i++) {
@@ -65,18 +50,21 @@ int main(int argc, char *argv[]){
         u[n+1] = 0;
         ft[0] = fprime[0];
         ft[1] = fprime[1];
-        bt[0] = b[0];
-        bt[1] = b[0];
+        bt[0] = 2.0;  // Diagonal values
+        bt[1] = 2.0;  // Diagonal values
 
         for(int i = 2; i < n+2; i++) { //n+1 eller n???
-                bt[i] = b[i] - a[i-1]*c[i-1]/bt[i-1];
-                ft[i] = fprime[i] - ft[i-1]*a[i-1]/bt[i-1];
+                bt[i] = 2.0 - 1.0/bt[i-1];
+                k[i-1] = ft[i-1]/bt[i-1];
+                ft[i] = fprime[i] + k[i-1];
         }
+        for(int i = n; i > 0; i--) {
+                u[i] = k[i] + u[i+1]/bt[i];
+        }
+
+        // Writing to file
         outfile.open(filename);
         //outfile << "  x:        approx:          exact:       relative error:" << endl;
-        for(int i = n; i > 0; i--) {
-                u[i] = (ft[i] - c[i]*u[i+1])/bt[i];
-        }
         for(int i=0; i < n+2; i++) {
                 outfile << x[i];
                 outfile << " " << u[i];
