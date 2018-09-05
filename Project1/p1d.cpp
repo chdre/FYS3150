a1cf41e386d50a97e1e6cbdfa208f877e54dafe9
chdre <christerdr@outlook.com>
2018-09-05 18:27:07 +0200

    Implemented eps_i

--- Project1/p1d.cpp
+++ Project1/p1d.cpp
@@ -32,12 +32,13 @@ int main(int argc, char *argv[]){
         double h = 1.0/(n+1); // Step size
 
         // Vectors for calculations
-        double*x = new double [n+2]; // x_i's
-        double*k = new double [n+2];  // (f tilde)/(b tilde)
+        double*x = new double [n+2];      // x_i's
+        double*k = new double [n+2];      // (f tilde)/(b tilde)
         double*u = new double [n+2];
-        double*bt = new double [n+2]; // b tilde, diagonal elements (NOT RHS of equation)
-        double*ft = new double [n+2]; // f tilde (RHS of equation)
+        double*bt = new double [n+2];     // b tilde, diagonal elements (NOT RHS of equation)
+        double*ft = new double [n+2];     // f tilde (RHS of equation)
         double*fprime = new double [n+2]; // our function f multiplied by h²
+        double*eps = new double [n];      // relative error
 
         //loop to update
         for(int i = 0; i < n+2; i++) {
@@ -63,13 +64,17 @@ int main(int argc, char *argv[]){
         for(int i = n; i > 0; i--) {
                 u[i] = (fprime[i] + u[i+1])/bt[i];
         }
+        for(int i = 1; i < n+1; i++) {
+                eps[i] = log10(abs((u[i] - ft[i])/ft[i]))
+        }
         // Writing to file
         outfile.open(filename);
         //outfile << "  x:        approx:          exact:       relative error:" << endl;
         for(int i=0; i < n+2; i++) {
                 outfile << x[i];
                 outfile << " " << u[i];
-                outfile << " " << closed_form(x[i]) << endl;
+                outfile << " " << closed_form(x[i]);
+                outfile << " " << eps[i] << endl;
         }
         outfile.close();
         return 0;
