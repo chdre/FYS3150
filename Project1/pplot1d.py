9cbadb60b661ac165a007858fcfcff444b285121
chdre <christerdr@outlook.com>
2018-09-10 12:14:40 +0200

    Plot for 1d

--- Project1/pplot1d.py
+++ Project1/pplot1d.py
@@ -12,7 +12,8 @@ for lines in open(infile, "r"):
     log10h.append(float(col[1]))
 
 plt.figure()
+plt.title('Logarithmic plot of error as a function of step size h',size=18)
 plt.plot(log10h,eps)
-plt.xlabel('$\\log_{10}(h)$');plt.ylabel('$\\epsilon_i$')
-plt.legend(['$\\epsilon_i$'])
+plt.xlabel('$\\log_{10}(h)$', size=18);plt.ylabel('$\\epsilon_i$',size=18)
+plt.legend(['$\\epsilon_i$'],prop={'size': 18})
 plt.show()
