0ab182f8b209e0384ee468c608400b943923d2d3
chdre <christerdr@outlook.com>
2018-09-17 13:54:31 +0200

    Started work on project 2

--- Project2/2b.cpp
+++ Project2/2b.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
-#include <cmath>
-#include <string>
-#include <iomanip>
-#include <fstream>
 #include <armadillo>
+#include <cmath>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+  int N; // size of matrix
+
+  if (argc <= 1) {
+    cout << "Missing arguments:" << argv[0] << " specify size of matrix N"
+         << endl;
+    exit(1);
+  } else {
+    // reading value of N
+    N = atoi(argv[1]) // N from acsi
+  }
+
+  double h = 1.0 / (N + 1);
+}
