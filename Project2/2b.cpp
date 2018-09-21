ea4929523833560f1d35e9d9aa9bb8c46aad0d8a
chdre <christerdr@outlook.com>
2018-09-21 16:19:15 +0200

    Finished maxval

--- Project2/2b.cpp
+++ Project2/2b.cpp
@@ -8,6 +8,22 @@
 using namespace std;
 using namespace arma;
 
+double max_offdiag_test(mat &A, int n, double h) {
+  /* function to find the maximum value of the array A. The diagonal is set to
+   0. The index of the maximum value is found, since the function returns a
+   single number we must transform this to indices for the matrix.*/
+  mat A_temp = abs(A);
+  A_temp -= eye(size(A)) * (double(2.0) / pow(h, 2)); // diagonal = 0
+  A_temp(0, 1) = 0;
+  uword max_index = A_temp.index_max(); // finding index of max element
+  int l = int(max_index) / n;           // find correct index for column
+  int k = int(max_index) - (n * l);     // find correct index for row
+  double maxelm = A_temp(k, l);         // max element
+  // cout << A_temp << endl;    // printing array
+  // cout << k << " " << l << endl; // printing indices
+  return maxelm;
+}
+
 main(int argc, char *argv[]) {
   int n; // size of matrix
 
@@ -16,40 +32,21 @@ main(int argc, char *argv[]) {
          << endl;
     exit(1);
   } else {
-    // reading value of N
-    n = atoi(argv[1]); // N from asci
+    n = atoi(argv[1]); // N from as ci
   }
 
   double h = 1.0 / (n + 1); // step length
   double eps = 1.0e-8;      // tolerance
 
   // Creating tridiagonal matrix
-  mat A = zeros<mat>(n + 2, n + 2);      // matrix for A
-  A.diag() += double(2.0) / pow(h, 2);   // center diagonal
-  A.diag(1) -= double(1.0) / pow(h, 2);  // upper diagonal
-  A.diag(-1) -= double(1.0) / pow(h, 2); // lower diagonal
+  mat A = zeros<mat>(n, n);             // matrix for A
+  A.diag() += double(2.0) / pow(h, 2);  // center diagonal
+  A.diag(1) -= double(1.0) / pow(h, 2); // upper diagonal
+  // A.diag(-1) -= double(1.0) / pow(h, 2); // lower diagonal
 
   // creating a while loop that checks whether the off diagonal elements are
   // larger than eps
-  while (maxelm > eps) {
-  }
-
-  double max_offdiag_test(double *maxelm, int *k, int *l) {
-    // function to find the maximum value of
-    mat A_temp = A.diag() *= 0.0;         // diagonal = 0
-    uword max_index = A_temp.index_max(); // finding index of max element
-    *maxelm = A_temp(max_index);          // max element
-  }
-
-  double max_offdiag(double *maxelm, ) {
-    *maxelm = A[1, 1];
-    for (int i = 1; i < n; i++) {
-      for (int j = 1; j < n; j++) {
-        if (abs(A[i, j]) > maxelm) { // checking whether
-          *maxelm = abs(A[i, j]);
-        }
-      }
-    }
-  }
+  // while (maxelm > eps) {
+  //}
   return 0;
 }
