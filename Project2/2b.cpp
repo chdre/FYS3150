42697a8ec003b6d5253b590d43cc9882937f1db0
chdre <38704494+chdre@users.noreply.github.com>
2018-09-23 13:32:02 +0200

    Starting on jacobi's method

--- Project2/2b.cpp
+++ Project2/2b.cpp
@@ -8,21 +8,27 @@
 using namespace std;
 using namespace arma;
 
-double max_offdiag_test(mat &A, int n, double h) {
+double max_offdiag(mat &A, int n, double h) {
   /* function to find the maximum value of the array A. The diagonal is set to
    0. The index of the maximum value is found, since the function returns a
    single number we must transform this to indices for the matrix.*/
-  mat A_temp = abs(A);
-  A_temp -= eye(size(A)) * (double(2.0) / pow(h, 2)); // diagonal = 0
-  A_temp(0, 1) = 0;
+  mat A_temp = abs(A);  // absolute value of all elements in array
+  A_temp -= eye(size(A)) * (double(2.0) / pow(h, 2)); // setting diagonal = 0
   uword max_index = A_temp.index_max(); // finding index of max element
   int l = int(max_index) / n;           // find correct index for column
   int k = int(max_index) - (n * l);     // find correct index for row
   double maxelm = A_temp(k, l);         // max element
-  // cout << A_temp << endl;    // printing array
-  // cout << k << " " << l << endl; // printing indices
   return maxelm;
 }
+
+void jacobi(double ** A){
+  // creating a while loop that checks whether the off diagonal elements are
+  // larger than eps. Calling the function max_offdiag to find.
+  while (max_offdiag > eps) {
+
+ }
+}
+
 
 main(int argc, char *argv[]) {
   int n; // size of matrix
@@ -39,14 +45,9 @@ main(int argc, char *argv[]) {
   double eps = 1.0e-8;      // tolerance
 
   // Creating tridiagonal matrix
-  mat A = zeros<mat>(n, n);             // matrix for A
+  mat * A = zeros<mat>(n, n);             // matrix for A
   A.diag() += double(2.0) / pow(h, 2);  // center diagonal
   A.diag(1) -= double(1.0) / pow(h, 2); // upper diagonal
-  // A.diag(-1) -= double(1.0) / pow(h, 2); // lower diagonal
-
-  // creating a while loop that checks whether the off diagonal elements are
-  // larger than eps
-  // while (maxelm > eps) {
-  //}
+  A.diag(-1) -= double(1.0) / pow(h, 2); // lower diagonal
   return 0;
 }
