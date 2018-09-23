8286e56f26614cd6dc0c197bfe465cbf5d60066c
chdre <christerdr@outlook.com>
2018-09-23 18:39:37 +0200

    Corr

--- Project2/2b.cpp
+++ Project2/2b.cpp
@@ -9,22 +9,6 @@
 using namespace std;
 using namespace arma;
 
-double max_offdiag(mat &A, int n, double h, int *l, int *k) {
-  /* function to find the maximum value of the array A. The diagonal is set to
-   0. The index of the maximum value is found, since the function returns a
-   single number we must transform this to indices for the matrix.*/
-  double maxelm = 0.0;
-  mat A_temp = abs(A); // absolute value of all elements in array
-  A_temp -= eye(size(A)) * (double(2.0) / pow(h, 2)); // setting diagonal = 0
-  uword max_index = A_temp.index_max(); // finding index of max element
-  cout << typeid(max_index) << endl;
-  *l = int(max_index) / n; // find correct index for column
-  cout << typeid(l) << endl;
-  *k = int(max_index) - (n * l); // find correct index for row
-  maxelm = A_temp(k,l)         // max element
-  return maxelm;
-}
-
 void jacobi(mat &A, mat &R, int n, double h) {
   // declaring variables needed
   double eps = 1.0e-8; // tolerance
@@ -35,8 +19,9 @@ void jacobi(mat &A, mat &R, int n, double h) {
   int iter = 0;                     // counter for iterations
   // creating a while loop that checks whether the off diagonal elements are
   // larger than eps. Calling the function max_offdiag to find.
-  while (max_offdiagval > eps && iter <) {
+  while (max_offdiagval > eps && iter < max_iter) {
     max_offdiagval = maxoffdiag(A, n, h, &l, &k);
+    rotate(A, R, k, l, n);
     iter++;
   }
 }
@@ -45,8 +30,8 @@ void rotate(mat &A, mat &R, int k, int l, int n) {
   // rotating the matrix A, saving eigenvalues in vector R
   double c, s, tau, t;
 
-  tau = (A(l,l)- A(k,k)) / (2 * A(k,l)); // tau = cot2\theta
-  if (A(k,l) !=
+  tau = (A(l, l) - A(k, k)) / (2 * A(k, l)); // tau = cot2\theta
+  if (A(k, l) !=
       0.0) { // making sure we do not divide by 0 (orthognal rotation)
     if (tau > 0) {
       t = -tau + sqrt(1 + pow(tau, 2));
@@ -60,16 +45,16 @@ void rotate(mat &A, mat &R, int k, int l, int n) {
   }
 
   // changing matrix elements
-  A(k,k)= pow(c, 2) * A(k,k)- 2.0 * c * s * A(k,l)+ pow(s, 2) * A(l,l);
-  A(l,l)= pow(s, 2) * A(k,k)+ 2.0 * c * s * A(k,l)+ pow(c, 2) * A(l,l);
-  A(k,l)= 0.0;
-  A(l,k)= 0.0;
+  A(k, k) = pow(c, 2) * A(k, k) - 2.0 * c * s * A(k, l) + pow(s, 2) * A(l, l);
+  A(l, l) = pow(s, 2) * A(k, k) + 2.0 * c * s * A(k, l) + pow(c, 2) * A(l, l);
+  A(k, l) = 0.0;
+  A(l, k) = 0.0;
   // change remaining elements
   for (int i = 0; i < n; i++) {
     if (i != k && i != l) {
-      A(i, k) = c * A(i,k) - s * A(i,l);
-      A(k, i) = A(i,k);
-      A(i, l) = c * A(i, l) + s * A(i,k);
+      A(i, k) = c * A(i, k) - s * A(i, l);
+      A(k, i) = A(i, k);
+      A(i, l) = c * A(i, l) + s * A(i, k);
       A(l, i) = A(i, l);
     }
     // compute eingenvectors
@@ -77,6 +62,22 @@ void rotate(mat &A, mat &R, int k, int l, int n) {
     R(i, l) = c * R(i, l) + s * R(i, k);
   }
   return;
+}
+
+double max_offdiag(mat &A, int n, double h, int *l, int *k) {
+  /* function to find the maximum value of the array A. The diagonal is set to
+   0. The index of the maximum value is found, since the function returns a
+   single number we must transform this to indices for the matrix.*/
+  double maxelm = 0.0;
+  mat A_temp = abs(A); // absolute value of all elements in array
+  A_temp -= eye(size(A)) * (double(2.0) / pow(h, 2)); // setting diagonal = 0
+  uword max_index = A_temp.index_max(); // finding index of max element
+  // cout << typeid(max_index) << endl;;
+  *l = max_index / n; // find correct index for column
+  // cout << typeid(l) << endl;
+  *k = max_index - (n * l); // find correct index for row
+  maxelm = A_temp(k, l)     // max element
+      return maxelm;
 }
 
 main(int argc, char *argv[]) {
