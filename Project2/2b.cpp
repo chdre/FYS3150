2654e93a64386bc19f651aa94a0f0a737d4d5600
chdre <christerdr@outlook.com>
2018-09-24 13:21:27 +0200

    Getting wrong eigvals

--- Project2/2b.cpp
+++ Project2/2b.cpp
@@ -11,103 +11,103 @@
 using namespace std;
 using namespace arma;
 
+ofstream outfile;
+
 double max_offdiag(mat &A, int n, double h, int *l, int *k) {
-  /* function to find the maximum value of the array A. The diagonal is set to
-   0. The index of the maximum value is found, since the function returns a
-   single number we must transform this to indices for the matrix.*/
+  // function to find the maximum value of the array A
   double maxelm = 0.0;
-  mat A_temp = abs(A); // absolute value of all elements in array
-  A_temp -= eye(size(A)) * (double(2.0) / pow(h, 2)); // setting diagonal = 0
 
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n; j++) {
-      if (A(i, j) > maxelm) {
-        maxelm = A(i, j);
+    for (int j = i + 1; j < n; j++) {
+      if (abs(A(i, j)) > maxelm) {
+        maxelm = abs(A(i, j));
         *l = i;
         *k = j;
       }
     }
   }
-
-  // THROWING THIS SOLUTION
-  // uword max_index_temp = A_temp.index_max(); // finding index of max
-  // element
-  // double maxelm = max_element(A_temp, A_temp + n);
-  //  int max_index_temp = distance(A_temp, max_element(A_temp, A_temp + n));
-  // cout << max_index_temp << endl;
-  // int max_index = reverse_iterator<iter_type> *l = max_index / n;
-  // find correct index for column
-  //*k = max_index - (n * l);   // find correct  index for row
-  // maxelm = A_temp(max_index); // max element
   return maxelm;
 }
 
-void rotate(mat &A, mat &R, int k, int l, int n) {
+void rotate(mat &A, mat &R, int &k, int &l, int n) {
   // rotating the matrix A, saving eigenvalues in vector R
-  double c, s, tau, t;
+  double c, s, t, tau;
 
-  tau = (A(l, l) - A(k, k)) / (2 * A(k, l)); // tau = cot2\theta
-  if (A(k, l) !=
-      0.0) { // making sure we do not divide by 0 (orthognal rotation)
-    if (tau > 0) {
-      t = -tau + sqrt(1 + pow(tau, 2));
+  if (A(k, l) != 0.0) { // making sure we do not divide by 0
+    tau = (A(l, l) - A(k, k)) / (2 * A(k, l)); // tau = cot2\theta
+    if (tau >= 0) {
+      t = 1.0 / (tau + sqrt(1 + pow(tau, 2)));
     } else {
-      t = tau + sqrt(1 + pow(tau, 2));
+      t = -1.0 / (-tau + sqrt(1 + pow(tau, 2))); // tau + sqrt(1 + pow(tau, 2));
     }
-    c = 1.0 / sqrt(1 + pow(tau, 2));
+    c = 1.0 / sqrt(1 + pow(t, 2));
+    s = t * c;
   } else { // rotation is orthogonal
     c = 1.0;
     s = 0.0;
   }
+  double A_ll = A(l, l);
+  double A_kk = A(k, k);
+  double A_kl = A(k, l);
 
   // changing matrix elements
-  A(k, k) = pow(c, 2) * A(k, k) - 2.0 * c * s * A(k, l) + pow(s, 2) * A(l, l);
-  A(l, l) = pow(s, 2) * A(k, k) + 2.0 * c * s * A(k, l) + pow(c, 2) * A(l, l);
+  A(k, k) = pow(c, 2) * A_kk - 2.0 * c * s * A_kl + pow(s, 2) * A_ll;
+  A(l, l) = pow(s, 2) * A_kk + 2.0 * c * s * A_kl + pow(c, 2) * A_ll;
   A(k, l) = 0.0;
   A(l, k) = 0.0;
   // change remaining elements
   for (int i = 0; i < n; i++) {
     if (i != k && i != l) {
-      A(i, k) = c * A(i, k) - s * A(i, l);
-      A(k, i) = A(i, k);
-      A(i, l) = c * A(i, l) + s * A(i, k);
-      A(l, i) = A(i, l);
+      double A_ik = A(i, k);
+      double A_il = A(i, l);
+
+      A(i, k) = c * A_ik - s * A_il;
+      A(k, i) = A_ik;
+      A(i, l) = c * A_il + s * A_ik;
+      A(l, i) = A_il;
     }
-    // compute eingenvectors
-    R(i, k) = c * R(i, k) - s * R(i, l);
-    R(i, l) = c * R(i, l) + s * R(i, k);
+    // compute eigenvectors
+    double R_il = R(i, l);
+    double R_ik = R(i, k);
+
+    R(i, k) = c * R_ik - s * R_il;
+    R(i, l) = c * R_il + s * R_ik;
   }
-  cout << R << endl;
   return;
 }
 
 void jacobi(mat &A, mat &R, int n, double h) {
-  // declaring variables needed
-  double eps = 1.0e-8; // tolerance
   int k, l;            // indices for largest off diagonal element
+  double eps = 1.0e-8; // tolerance
+
+  R = zeros<mat>(n, n); // eigenvector matrix
+  R.diag() += double(1.0);
 
   double max_offdiagval = max_offdiag(A, n, h, &l, &k); // max offdiag element
   int max_iter = pow(double(n), 3); // max number of iterations
   int iter = 0;                     // counter for iterations
   // creating a while loop that checks whether the off diagonal elements are
   // larger than eps. Calling the function max_offdiag to find.
-  while (max_offdiagval > eps && iter < max_iter) {
+  while (abs(max_offdiagval) > eps && iter < max_iter) {
     max_offdiagval = max_offdiag(A, n, h, &l, &k);
     rotate(A, R, k, l, n);
     iter++;
   }
+  cout << "Number of iterations: " << iter << "\n";
   return;
 }
 
 main(int argc, char *argv[]) {
-  int n; // size of matrix
+  int n;           // size of matrix
+  string filename; // filename for output file
 
   if (argc <= 1) {
-    cout << "Missing arguments:" << argv[0] << " specify size of matrix N"
-         << endl;
+    cout << "Missing arguments:" << argv[0]
+         << " specify filename for output and size n of matrices" << endl;
     exit(1);
   } else {
-    n = atoi(argv[1]); // N from as ci
+    filename = argv[1];
+    n = atoi(argv[2]); // N from as ci
   }
 
   double h = 1.0 / (n + 1); // step length
@@ -118,16 +118,26 @@ main(int argc, char *argv[]) {
   A.diag(1) -= double(1.0) / pow(h, 2);  // upper diagonal
   A.diag(-1) -= double(1.0) / pow(h, 2); // lower diagonal
 
-  mat R = zeros<mat>(n, n); // eigenvector matrix
-  R.diag() += double(1.0);
+  mat R;
+
+  vec eigval;
+  mat eigvec;
+
+  eig_sym(eigval, eigvec, A);
 
   jacobi(A, R, n, h);
 
-  cout << R << endl;
-
-  vec eigvals = eig_sym(A);
-
-  cout << eigvals << endl;
+  // writing to file
+  outfile.open(filename);
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      if (i == j) {
+        outfile << R(i, j);
+        outfile << " " << eigval[i] << endl;
+      }
+    }
+  }
+  outfile.close();
 
   return 0;
 }
