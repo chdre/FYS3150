1e284ff469fb27a28419673bef14dfc9d14f0d4d
chdre <christerdr@outlook.com>
2018-12-07 16:26:29 +0100

    Commitment issues

--- Project5/2D/BackEuler.cpp
+++ Project5/2D/BackEuler.cpp
@@ -1,36 +1,41 @@
 #include "BackEuler.hpp"
 
 void BESolver(int n, double alpha, int tmax){
-        vec u = zeros<vec>(n+2);  // Au = r
-        vec r = zeros<vec>(n+2);  // vector for current time step of Crank-Nicolson
+        mat u = zeros<mat>(n+2,n+2);  // Au = r
+        mat r = zeros<mat>(n+2,n+2);  // vector for current time step of Crank-Nicolson
 
         // Boundary conditions (u(0) set by zeros)
-        u(n+1) = 1.0;
+        for(int i = 0; i < n+2; i++) {
+                u(n+1,i) = 1.0;
+        }
 
         // Matrix elements of tridiagonal matrix
         double e = -alpha;
-        double d = 1.0 + 2.0*alpha;
+        double d = 1.0 + 4.0*alpha;
 
         ofstream outfile;
-        outfile.open("BWEuler.txt");
+        outfile.open("BWEuler2D.txt");
 
         for(int j = 1; j < tmax; j++) {
                 tridiagonal(d, e, u, r, n);
 
                 // Preserving boundary conditions
-                u(0) = 0.0;
-                u(n+1) = 1.0;
-                r(0) = 0.0;
-                r(n+1) = 1.0;
+                for(int i = 0; i < n+2; i++) {
+                        u(0,i) = 0;
+                        u(n+1,i) = 1.0;
+                        r(0,i) = 0.0;
+                        r(n+1,i) = 1.0;
+                }
 
                 r = u;  // Setting right hand side of equation to u, for all i
 
                 // writing to file
-                outfile << j << " ";
                 for(int i = 0; i < n+2; i++) {
-                        outfile << u(i) << " ";
+                        for(int j = 0, j < n+2; j++) {
+                                outfile << u(i,j) << " ";
+                        }
+                        outfile << endl;
                 }
-                outfile << endl;
         }
         outfile.close();
 
